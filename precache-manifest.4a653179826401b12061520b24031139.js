2d3cd70b1f0b2df7715f5059cafe47b9d9f105d0
Samar Ijaz <63924454+samarijaz@users.noreply.github.com>
2020-10-15 21:30:10 +0200

    Updates

---
+++ precache-manifest.4a653179826401b12061520b24031139.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e77e6abca70fdd612c801dac88cc77d",
+    "url": "/Face-Detection-Machine-Learning/index.html"
+  },
+  {
+    "revision": "fa20a8c05f38b54934fe",
+    "url": "/Face-Detection-Machine-Learning/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ec44d99a7226192d9f6b",
+    "url": "/Face-Detection-Machine-Learning/static/css/main.4b3d2a42.chunk.css"
+  },
+  {
+    "revision": "fa20a8c05f38b54934fe",
+    "url": "/Face-Detection-Machine-Learning/static/js/2.27dcdcf3.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Face-Detection-Machine-Learning/static/js/2.27dcdcf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec44d99a7226192d9f6b",
+    "url": "/Face-Detection-Machine-Learning/static/js/main.ab98e1e2.chunk.js"
+  },
+  {
+    "revision": "c5a70be0a80841124f94",
+    "url": "/Face-Detection-Machine-Learning/static/js/runtime-main.71d8acd6.js"
+  }
+]);
